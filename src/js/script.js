13ba20d3f465a3e2264f7b0c366eac54e3ebb1dc
Tatiana <tvtugova1511@gmail.com>
2023-08-16 15:42:21 +0300

    lesson 59

--- src/js/script.js
+++ src/js/script.js
@@ -13,6 +13,42 @@ $(document).ready(function(){
             }
         ]
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this) 
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active') 
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            }) 
+        });
+    };
+
+   toggleSlide('.catalog-item__link'); 
+   toggleSlide('.catalog-item__back'); 
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
 }); 
 
 
@@ -33,3 +69,5 @@ $(document).ready(function(){
     slider.goTo('next'); */
 /* });  *//* Tiny-slider */ 
  /* Альтернативный способ записи, результат будет такой же, как и в прошлый раз */
+
+
